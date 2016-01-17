03aeac341c32e54297c1193f24d564bff1958e0e
Meric Garcia <meric.garcia@keyconsulting.fr>
2016-01-17 13:42:59 +0100

    First Version of the jsp

---
+++ script/user.sql
@@ -0,0 +1,14 @@
+﻿-- Table: "user"
+
+-- DROP TABLE "user";
+
+CREATE TABLE test."user"
+(
+  firstname character varying(255),
+  lastname character varying(255)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE test."user"
+  OWNER TO test;
